3ebb49e0a544b0f6005435df3e2a711fad13d4b5
delbertina <6349928+delbertina@users.noreply.github.com>
2024-10-08 22:03:47 -0500

    Cleanup testing implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,84 +2,60 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [gamepads, setGamepads] = useState<Gamepad[]>([]);
-  const rAF = window.requestAnimationFrame;
+  const [eventLog, setEventLog] = useState<string[]>([]);
+  // const [gamepads, setGamepads] = useState<Gamepad[]>([]);
+  // const rAF = window.requestAnimationFrame;
 
-  window.addEventListener("gamepadconnected", (evt) => {
-    setGamepads([...gamepads, evt.gamepad]);
-    rAF(updateStatus);
-  });
-  window.addEventListener("gamepaddisconnected", (evt) => {
-    setGamepads([...gamepads.filter((pad) => pad !== evt.gamepad)]);
-  });
-  window.addEventListener('gamepadbuttondown', (evt: Gamepad) => {
+  // window.addEventListener("gamepadconnected", (evt) => {
+  //   setGamepads([...gamepads, evt.gamepad]);
+  //   rAF(updateStatus);
+  // });
+  // window.addEventListener("gamepaddisconnected", (evt) => {
+  //   setGamepads([...gamepads.filter((pad) => pad !== evt.gamepad)]);
+  // });
+  // @ts-expect-error (Firefox experimental event)
+  window.addEventListener('gamepadbuttondown', (evt: {gamepad: Gamepad, button: number}) => {
     gamepadButtonDown(evt)
   });
 
-  const gamepadButtonDown = (e) => {
-    console.log('Gamepad button down at index %d: %s. Button: %d.',
-      e.gamepad.index, e.gamepad.id, e.button);
-  };
-
-  function updateStatus() {
-    scangamepads();
-    gamepads.forEach((pad) => {
-      pad.buttons.forEach((butt, i) => {
-
-      })
-    })
-    // gamepads.forEach((pad) => {
-    //   var d = document.getElementById("controller" + j);
-    //   var buttons = d.getElementsByClassName("button");
-    //   for (var i=0; i<controller.buttons.length; i++) {
-    //     var b = buttons[i];
-    //     var val = controller.buttons[i];
-    //     var pressed = val == 1.0;
-    //     var touched = false;
-    //     if (typeof(val) == "object") {
-    //       pressed = val.pressed;
-    //       if ('touched' in val) {
-    //         touched = val.touched;
-    //       }
-    //       val = val.value;
-    //     }
-    //     var pct = Math.round(val * 100) + "%";
-    //     b.style.backgroundSize = pct + " " + pct;
-    //     b.className = "button";
-    //     if (pressed) {
-    //       b.className += " pressed";
-    //     }
-    //     if (touched) {
-    //       b.className += " touched";
-    //     }
-    //   }
-
-    //   var axes = d.getElementsByClassName("axis");
-    //   for (var i=0; i<controller.axes.length; i++) {
-    //     var a = axes[i];
-    //     a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
-    //     a.setAttribute("value", controller.axes[i]);
-    //   }
-    // }
-    rAF(updateStatus);
+  const gamepadButtonDown = (e: {gamepad: Gamepad, button: number}) => {
+    const newEvent = `Gamepad button down at index ${e.gamepad.index}: ${e.gamepad.id}. Button: ${e.button}.`;
+    console.log(newEvent);
+    setEventLog([newEvent, ...eventLog]);
   }
 
-  function scangamepads() {
-    const pads = navigator.getGamepads ? navigator.getGamepads() : [];
-    for (let i = 0; i < gamepads.length; i++) {
-      // @ts-expect-error bad
-      if (pads[i] != null && pads[i].index in gamepads) {
-        // @ts-expect-error bad
-        gamepads[pads[i].index] = pads[i];
-      }
-    }
-  }
+  // function updateStatus() {
+  //   scangamepads();
+  //   gamepads.forEach((pad) => {
+  //     pad.buttons.forEach((butt, i) => {
 
-  setInterval(scangamepads, 500);
+  //     })
+  //   })
+  //   rAF(updateStatus);
+  // }
+
+  // function scangamepads() {
+  //   const pads = navigator.getGamepads ? navigator.getGamepads() : [];
+  //   for (let i = 0; i < gamepads.length; i++) {
+  //     // @ts-expect-error bad
+  //     if (pads[i] != null && pads[i].index in gamepads) {
+  //       // @ts-expect-error bad
+  //       gamepads[pads[i].index] = pads[i];
+  //     }
+  //   }
+  // }
+
+  // setInterval(scangamepads, 500);
 
   return (
     <>
       <h1 className="font-bold">Testing Gamepad Inputs</h1>
+      {eventLog.map((log, i) => (
+        <div key={i}>
+          {log}
+        </div>
+      ))}
+      {/*
       {gamepads.map((pad) => (
         <div key={pad.index}>
           {pad.index}
@@ -92,6 +68,7 @@ function App() {
           ))}
         </div>
       ))}
+      */}
     </>
   );
 }
