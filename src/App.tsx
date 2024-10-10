8c5f5801ce530ae103de88eb986d4970eece871f
delbertina <6349928+delbertina@users.noreply.github.com>
2024-10-09 21:38:26 -0500

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,7 @@ import "./App.css";
 
 function App() {
   const [eventLog, setEventLog] = useState<string[]>([]);
-  // const [gamepads, setGamepads] = useState<Gamepad[]>([]);
-  // const rAF = window.requestAnimationFrame;
 
-  // window.addEventListener("gamepadconnected", (evt) => {
-  //   setGamepads([...gamepads, evt.gamepad]);
-  //   rAF(updateStatus);
-  // });
-  // window.addEventListener("gamepaddisconnected", (evt) => {
-  //   setGamepads([...gamepads.filter((pad) => pad !== evt.gamepad)]);
-  // });
   // @ts-expect-error (Firefox experimental event)
   window.addEventListener('gamepadbuttondown', (evt: {gamepad: Gamepad, button: number}) => {
     gamepadButtonDown(evt)
@@ -24,29 +15,6 @@ function App() {
     setEventLog([newEvent, ...eventLog]);
   }
 
-  // function updateStatus() {
-  //   scangamepads();
-  //   gamepads.forEach((pad) => {
-  //     pad.buttons.forEach((butt, i) => {
-
-  //     })
-  //   })
-  //   rAF(updateStatus);
-  // }
-
-  // function scangamepads() {
-  //   const pads = navigator.getGamepads ? navigator.getGamepads() : [];
-  //   for (let i = 0; i < gamepads.length; i++) {
-  //     // @ts-expect-error bad
-  //     if (pads[i] != null && pads[i].index in gamepads) {
-  //       // @ts-expect-error bad
-  //       gamepads[pads[i].index] = pads[i];
-  //     }
-  //   }
-  // }
-
-  // setInterval(scangamepads, 500);
-
   return (
     <>
       <h1 className="font-bold">Testing Gamepad Inputs</h1>
@@ -55,20 +23,6 @@ function App() {
           {log}
         </div>
       ))}
-      {/*
-      {gamepads.map((pad) => (
-        <div key={pad.index}>
-          {pad.index}
-          {pad.buttons.map((butt, i) => (
-            <div key={i}>
-              {i}
-              {butt.pressed ? "True" : "False"}
-              {butt.touched ? "True" : "False"}
-            </div>
-          ))}
-        </div>
-      ))}
-      */}
     </>
   );
 }
