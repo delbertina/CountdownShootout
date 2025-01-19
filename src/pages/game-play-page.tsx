2561cdd9d7103fb17429fa29889cbb45f1f3b9ec
delbertina <6349928+delbertina@users.noreply.github.com>
2025-01-19 00:07:41 -0600

    Finish implementing new layout, need to refine

--- src/pages/game-play-page.tsx
+++ src/pages/game-play-page.tsx
@@ -1,14 +1,11 @@
 import { useMemo, useState } from "react";
 import { useGameStore } from "../store/gameStore";
 import { OnProgressProps } from "react-player/base";
-import GameHeader from "../components/game-header";
 import ReactPlayer from "react-player";
 import { Progress } from "../components/ui/progress";
 import { Dialog, DialogContent } from "../components/ui/dialog";
 import { GameStage } from "../types/game_types";
 import { getTeamDisplayName } from "../types/state_types";
-import ShadedIndicator from "../components/shaded-indicator";
-import { padNumber } from "../lib/utils";
 import GameHeaderLeft from "../components/ui/header/game-header-side";
 import GameHeaderTop from "../components/ui/header/game-header-top";
 
@@ -63,6 +60,7 @@ const GamePlayPage = () => {
         <>
           <div className="flex flex-row h-full w-full gap-4 grow">
             <GameHeaderLeft
+              isLeft={true}
               teams={teams.filter((_team, i) => i % 2 === 0)}
               isAnswering={isAnswering}
               isSuddenDeath={isSuddenDeath}
@@ -71,8 +69,11 @@ const GamePlayPage = () => {
             <div className="flex flex-col grow gap-4">
               <GameHeaderTop
                 headerTitle={currentGame?.title ?? ""}
-                headerSubtitle={questionId + 1 + "/" + currentGame?.questions.length}
-                headerBody={gameQuestion.questionText} />
+                headerSubtitle={
+                  questionId + 1 + "/" + currentGame?.questions.length
+                }
+                headerBody={gameQuestion.questionText}
+              />
               <div className="flex-grow flex flex-col justify-end w-full gap-4">
                 <div>
                   <ReactPlayer
@@ -108,124 +109,118 @@ const GamePlayPage = () => {
                 </div>
               </div>
             </div>
-            
-            <div className="flex flex-col gap-4">
-              {teams
-                .filter((_team, i) => i % 2 === 1)
-                .map((team) => (
-                  <div className="flex flex-row gap-4">
-                    <div className="text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                      <h1>
-                        {padNumber(
-                          team.scoreHistory.reduce(
-                            (sum, current) => sum + current,
-                            0
-                          ),
-                          2
-                        )}
-                      </h1>
-                    </div>
-                    <ShadedIndicator
-                      text={"BUZZER"}
-                      theme={team.theme}
-                      isShaded={
-                        // if the team can answer,
-                        // nobody is answering currently,
-                        // and it's either sudden death or the playing stage
-                        team.canAnswer &&
-                        !isAnswering &&
-                        (isSuddenDeath || gameStage === GameStage.Playing)
-                      }
-                    />
-                  </div>
-                ))}
-            </div>
+            <GameHeaderLeft
+              isLeft={true}
+              teams={teams.filter((_team, i) => i % 2 === 1)}
+              isAnswering={isAnswering}
+              isSuddenDeath={isSuddenDeath}
+              gameStage={gameStage}
+            />
           </div>
           <Dialog open={gameStage !== GameStage.Playing}>
             <DialogContent className="border-none rounded-none min-w-[100%] min-h-[100%] flex flex-col items-center gap-4 flex-grow text-center bg-slate-700 text-amber-200">
-              <GameHeader
-                indicatorText="BUZZER"
-                headerTitle={currentGame?.title ?? ""}
-                headerSubtitle={
-                  questionId + 1 + "/" + currentGame?.questions.length
-                }
-              />
-              {gameStage === GameStage.Waiting && (
-                <div>
-                  <h1>{gameQuestion.questionText}</h1>
-                  <h2>
-                    {gameQuestion.videoEndTime - gameQuestion.videoStartTime}{" "}
-                    seconds
-                  </h2>
-                  {/* timer for remaining time before question starts */}
-                </div>
-              )}
-              {gameStage === GameStage.Answering && (
-                <div>
-                  {isSuddenDeath && (
+              <div className="flex flex-row h-full w-full gap-4 grow">
+                <GameHeaderLeft
+                  isLeft={true}
+                  teams={teams.filter((_team, i) => i % 2 === 0)}
+                  isAnswering={isAnswering}
+                  isSuddenDeath={isSuddenDeath}
+                  gameStage={gameStage}
+                />
+                <div className="flex flex-col grow gap-4">
+                  <GameHeaderTop
+                    headerTitle={currentGame?.title ?? ""}
+                    headerSubtitle={
+                      questionId + 1 + "/" + currentGame?.questions.length
+                    }
+                    headerBody={gameQuestion.questionText}
+                  />
+                  {gameStage === GameStage.Waiting && (
+                    <div>
+                      <h1>{gameQuestion.questionText}</h1>
+                      <h2>
+                        {gameQuestion.videoEndTime -
+                          gameQuestion.videoStartTime}{" "}
+                        seconds
+                      </h2>
+                      {/* timer for remaining time before question starts */}
+                    </div>
+                  )}
+                  {gameStage === GameStage.Answering && (
+                    <div>
+                      {isSuddenDeath && (
+                        <>
+                          <h1>SUDDEN DEATH</h1>
+                          <br />
+                          <hr />
+                          <br />
+                        </>
+                      )}
+                      {teams
+                        .filter((team) => !!team.isAnswering)
+                        .map((team) => (
+                          <h1>{getTeamDisplayName(team)} is answering</h1>
+                        ))}
+                      {!isAnswering && <h1>Nobody is answering ... ?</h1>}
+                      {/* timer for remaining time to answer */}
+                    </div>
+                  )}
+                  {gameStage === GameStage.Scoring && (
                     <>
-                      <h1>SUDDEN DEATH</h1>
-                      <br />
+                      <div>
+                        {teams
+                          .filter((team) => !!team.isAnswering)
+                          .map((team) => (
+                            <h1>{getTeamDisplayName(team)} is correct</h1>
+                          ))}
+                        {!isAnswering && <h1>Nobody won the points</h1>}
+                        {/* timer for remaining time before next stage */}
+                      </div>
                       <hr />
-                      <br />
+                      <div>
+                        <h1>{gameQuestion.answer}</h1>
+                        <h2>{gameQuestion.answerSubtext}</h2>
+                      </div>
                     </>
                   )}
-                  {teams
-                    .filter((team) => !!team.isAnswering)
-                    .map((team) => (
-                      <h1>{getTeamDisplayName(team)} is answering</h1>
-                    ))}
-                  {!isAnswering && <h1>Nobody is answering ... ?</h1>}
-                  {/* timer for remaining time to answer */}
-                </div>
-              )}
-              {gameStage === GameStage.Scoring && (
-                <>
-                  <div>
-                    {teams
-                      .filter((team) => !!team.isAnswering)
-                      .map((team) => (
-                        <h1>{getTeamDisplayName(team)} is correct</h1>
-                      ))}
-                    {!isAnswering && <h1>Nobody won the points</h1>}
-                    {/* timer for remaining time before next stage */}
-                  </div>
-                  <hr />
-                  <div>
-                    <h1>{gameQuestion.answer}</h1>
-                    <h2>{gameQuestion.answerSubtext}</h2>
-                  </div>
-                </>
-              )}
-              {gameStage === GameStage.Ending && (
-                <div>
-                  <h1>Game Over</h1>
-                  <h2>
-                    {teams
-                      .filter(
-                        (team) =>
-                          team.scoreHistory.reduce(
+                  {gameStage === GameStage.Ending && (
+                    <div>
+                      <h1>Game Over</h1>
+                      <h2>
+                        {teams
+                          .filter(
+                            (team) =>
+                              team.scoreHistory.reduce(
+                                (sum, current) => sum + current,
+                                0
+                              ) === maxScore
+                          )
+                          .map((team) => (
+                            <h1>{getTeamDisplayName(team)} Won!</h1>
+                          ))}
+                      </h2>
+                      {teams.map((team) => (
+                        <>
+                          <h1>{getTeamDisplayName(team)} Score: </h1>
+                          {team.scoreHistory.reduce(
                             (sum, current) => sum + current,
                             0
-                          ) === maxScore
-                      )
-                      .map((team) => (
-                        <h1>{getTeamDisplayName(team)} Won!</h1>
+                          )}
+                          <br />
+                        </>
                       ))}
-                  </h2>
-                  {teams.map((team) => (
-                    <>
-                      <h1>{getTeamDisplayName(team)} Score: </h1>
-                      {team.scoreHistory.reduce(
-                        (sum, current) => sum + current,
-                        0
-                      )}
-                      <br />
-                    </>
-                  ))}
-                  {/* timer for remaining time before next stage */}
+                      {/* timer for remaining time before next stage */}
+                    </div>
+                  )}
                 </div>
-              )}
+                <GameHeaderLeft
+                  isLeft={true}
+                  teams={teams.filter((_team, i) => i % 2 === 1)}
+                  isAnswering={isAnswering}
+                  isSuddenDeath={isSuddenDeath}
+                  gameStage={gameStage}
+                />
+              </div>
             </DialogContent>
           </Dialog>
         </>
