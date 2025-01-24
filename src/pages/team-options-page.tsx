cf6109496483fb17fc99477b5c10d5ea77838425
delbertina <6349928+delbertina@users.noreply.github.com>
2025-01-23 22:46:18 -0600

    Update headers to be consistent - WIP add new game as later feature

--- src/pages/team-options-page.tsx
+++ src/pages/team-options-page.tsx
@@ -7,17 +7,19 @@ const TeamOptionsPage = () => {
   const teams = useGameStore((state) => state.teams);
   const addTeam = useGameStore((state) => state.addTeam);
   return (
-    <div className="whole-screen flex flex-col gap-4 justify-center bg-slate-300">
+    <div className="whole-screen flex flex-col gap-4 justify-start bg-slate-300">
       <div className="flex flex-row items-center justify-center gap-4">
         <div>
-      <Button disabled><Users /></Button>
+          <Button disabled>
+            <Users />
+          </Button>
         </div>
-      <h1 className="font-bold">Team Options</h1>
-      <div>
-        <Button onClick={() => addTeam()} disabled={teams.length >= 8}>
-          <Plus />
-        </Button>
-      </div>
+        <h1 className="font-bold">Team Options</h1>
+        <div>
+          <Button onClick={() => addTeam()} disabled={teams.length >= 8}>
+            <Plus />
+          </Button>
+        </div>
       </div>
       <hr />
       {/* Display the same section for each team */}
