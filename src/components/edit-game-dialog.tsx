7dc55e00256c0ed86979e370494119c2bcc843f0
delbertina <6349928+delbertina@users.noreply.github.com>
2025-03-03 23:38:41 -0600

    Layout edit game dialog

--- src/components/edit-game-dialog.tsx
+++ src/components/edit-game-dialog.tsx
@@ -12,10 +12,23 @@ const EditGameDialog = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Edit Game</DialogTitle>
-          <DialogDescription>
-            Edit the data of a game.
-          </DialogDescription>
+          <DialogDescription>Edit the data of a game.</DialogDescription>
         </DialogHeader>
+        <div>{/* Title */}</div>
+        <div>{/* Description */}</div>
+        <div>{/* Game Settings */}</div>
+        <div>
+          {/* Question List Header */}
+          {/* Add Question Button */}
+        </div>
+        <div>
+          {/* Questions List */}
+          <div>{/* Question Text */}</div>
+          <div>{/* Question Video */}</div>
+          <div>{/* Question Answer */}</div>
+          <div>{/* Question Answer Subtext */}</div>
+          <div>{/* Question Oncore */}</div>
+        </div>
       </DialogContent>
     </Dialog>
   );
