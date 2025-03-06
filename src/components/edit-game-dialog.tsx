af0ad48843be97460f1cb629d8f969215504c61a
delbertina <6349928+delbertina@users.noreply.github.com>
2025-03-05 23:52:49 -0600

    Copy paste in raw code from form builder site to see if this could work for what I want

--- src/components/edit-game-dialog.tsx
+++ src/components/edit-game-dialog.tsx
@@ -29,9 +29,403 @@ const EditGameDialog = () => {
           <div>{/* Question Answer Subtext */}</div>
           <div>{/* Question Oncore */}</div>
         </div>
+
       </DialogContent>
     </Dialog>
   );
 };
 
 export default EditGameDialog;
+
+
+"use client"
+import {
+  useState
+} from "react"
+import {
+  toast
+} from "sonner"
+import {
+  useForm
+} from "react-hook-form"
+import {
+  zodResolver
+} from "@hookform/resolvers/zod"
+import * as z from "zod"
+import {
+  cn
+} from "@/lib/utils"
+import {
+  Button
+} from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Input
+} from "@/components/ui/input"
+
+const formSchema = z.object({
+  game_title: z.string().min(1),
+  description: z.string().min(1),
+  info - timeout: z.number().min(1).max(30),
+  answer - timeout: z.number().min(1).max(30),
+  points - per - question: z.number().min(1).max(100),
+  question - text - 1: z.string().min(1),
+  question - video - src: z.string().min(1),
+  question - video - start - time - 1: z.number().min(0),
+  question - video - end - time - 1: z.number().min(0),
+  question - answer - 1: z.string().min(1),
+  question - answer - subtext - 1: z.string().min(1).optional(),
+  question - answer - oncore - src - 1: z.string().min(1).optional(),
+  question - answer - oncore - start - 1: z.number().min(0).optional(),
+  question - answer - oncore - end - 1: z.number().min(0).optional()
+});
+
+export default function MyForm() {
+
+  const form = useForm < z.infer < typeof formSchema >> ({
+    resolver: zodResolver(formSchema),
+
+  })
+
+  function onSubmit(values: z.infer < typeof formSchema > ) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+        
+        <FormField
+          control={form.control}
+          name="game_title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="New Game"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>The main name of the game.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="New game description"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>Any extra info about the game not included in the title.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <div className="grid grid-cols-12 gap-4">
+          
+          <div className="col-span-6">
+            
+        <FormField
+          control={form.control}
+          name="info-timeout"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Info Timeout</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="15"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>How long the info stage should be visable before advancing</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-6">
+            
+        <FormField
+          control={form.control}
+          name="answer-timeout"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Answer Timeout</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="15"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>How long teams get to answer</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+        </div>
+        
+        <FormField
+          control={form.control}
+          name="points-per-question"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Points Per Question</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="1"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>Max points each question is worth</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <FormField
+          control={form.control}
+          name="question-text-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Question Text</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="Question #1"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>Hint text for the question</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <div className="grid grid-cols-12 gap-4">
+          
+          <div className="col-span-4">
+            
+        <FormField
+          control={form.control}
+          name="question-video-src"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Question Video</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="youtube.com/???"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>Link to the YouTube for the question</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-4">
+            
+        <FormField
+          control={form.control}
+          name="question-video-start-time-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Start Time</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="15"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>Timestamp that the video should start playing in seconds</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-4">
+            
+        <FormField
+          control={form.control}
+          name="question-video-end-time-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>End Time</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="30"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>Timestamp that the video should end in seconds</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+        </div>
+        
+        <div className="grid grid-cols-12 gap-4">
+          
+          <div className="col-span-6">
+            
+        <FormField
+          control={form.control}
+          name="question-answer-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Answer</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="Question #1 Answer"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>The answer to the question</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-6">
+            
+        <FormField
+          control={form.control}
+          name="question-answer-subtext-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Answer Subtext</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder=""
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>Subtext for answer to the question</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+        </div>
+        
+        <div className="grid grid-cols-12 gap-4">
+          
+          <div className="col-span-4">
+            
+        <FormField
+          control={form.control}
+          name="question-answer-oncore-src-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Answer Oncore Video</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder=""
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>Youtube video URL for oncore video</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-4">
+            
+        <FormField
+          control={form.control}
+          name="question-answer-oncore-start-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Answer Oncore Start</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="15"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>Timestamp that the oncore video starts in seconds</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-4">
+            
+        <FormField
+          control={form.control}
+          name="question-answer-oncore-end-1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Answer Oncore End</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="30"
+                
+                type="number"
+                {...field} />
+              </FormControl>
+              <FormDescription>Time that the oncore video ends in seconds</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
